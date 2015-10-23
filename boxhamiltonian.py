b9ea924e2070badfb4f1a9335e70a83370480f15
Lee Worden <worden.lee@gmail.com>
2015-10-23 10:49:58 -0700

    lagrangian

--- boxhamiltonian.py
+++ boxhamiltonian.py
@@ -45,3 +45,56 @@ def hamiltonian_system_callback( self, N, km_states, bind_state, state_index, B,
 boxmodel.BoxModel.hamiltonian = lambda self, p_vars, reduce=False : boxkolmogorov.bm_kolmogorov_eqns( self, 1, hamiltonian_callback, p_vars, reduce )
 
 boxmodel.BoxModel.hamiltonian_system = lambda self, p_vars, reduce=False: boxkolmogorov.bm_kolmogorov_eqns( self, 1, hamiltonian_system_callback, p_vars, reduce )
+
+def lagrangian_callback( self, N, km_states, bind_state, state_index, B, p_vars, reduce=True, return_vars=False ):
+    # construct lagrangian function
+    if reduce and sum( self.ode_flow().values() ) == 0:
+	print 'Reducing dimension to', tuple(self._vars[:-1])
+	reduce = True
+	reduce_bindings = dynamicalsystems.Bindings( { self._vars[-1]: 1 - sum( self._vars[:-1] ) } )
+	if len(p_vars) == len(self._vars):
+	    reduce_bindings[p_vars[-1]] = 0 
+	#print reduce_bindings
+	p_vars = p_vars[:len(self._vars)-1]
+    else:
+	reduce = False
+	reduce_bindings = dynamicalsystems.Bindings()
+    H0 = sum(
+	reduce_bindings(r)
+	for s,t,r in self._flow_graph.edge_iterator()
+    )
+    H1 = sum(
+	reduce_bindings(r) * (
+	    exp( sum(
+		p*b for p,b in zip(
+		    p_vars,
+		    B[ state_index[t] ] - B[ state_index[s] ]
+		)
+	    ) )
+	)
+	for s,t,r in self._flow_graph.edge_iterator()
+    )
+    L1 = sum(
+	reduce_bindings(r) * 
+	    sum( p*b for p,b in zip(
+		    p_vars,
+		    B[ state_index[t] ] - B[ state_index[s] ]
+	    ) ) *
+	    exp( sum( p*b for p,b in zip(
+		p_vars,
+		B[ state_index[t] ] - B[ state_index[s] ]
+	    ) ) )
+	for s,t,r in self._flow_graph.edge_iterator()
+    )
+    L = H0 - H1 + L1
+    #for s,t,w in self._flow_graph.edge_iterator():
+	#r = B[ state_index[t] ] - B[ state_index[s] ]
+	#w = reduce_bindings(w)
+	#print 'r,w:', r, w
+	#H += w * (exp( sum( p*b for p,b in zip(p_vars,r) ) ) - 1 )
+    if return_vars:
+	return L, p_vars, reduce_bindings
+    else:
+	return L
+
+boxmodel.BoxModel.lagrangian = lambda self, p_vars, reduce=False : boxkolmogorov.bm_kolmogorov_eqns( self, 1, lagrangian_callback, p_vars, reduce )
