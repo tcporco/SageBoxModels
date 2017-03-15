d31b7dc18bf34a971f6cddab9c08a555e08cc441
Lee Worden <worden.lee@gmail.com>
2017-03-15 19:32:53 -0400

    text on free product of box models

--- boxmodel/__init__.py
+++ boxmodel/__init__.py
@@ -2,7 +2,7 @@
 from boxmodel import *
 from boxmodelproduct import *
 from boxkolmogorov import *
-from boxmarkov import *
+#from boxmarkov import *
 from boxhamiltonian import *
 from code_generation import *
 import graph_latex_patched
